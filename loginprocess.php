41a2992dcada4077e4da3371c317de93770defd3
tayabatariq <168851346+tayabatariq@users.noreply.github.com>
2024-05-17 17:29:27 +0500

    6

--- loginprocess.php
+++ loginprocess.php
@@ -1,5 +1,5 @@
 <?php
-print("hi");
+// print("hi");
 print($_POST["email"] . "<br>" . $_POST["userPassword"]);
 $serverName="localhost";
 $userName="root";
@@ -14,26 +14,7 @@ if($conn->connect_error){
 $sql="select * from studentInfo";
 
 $result=$conn->query($sql);
-    echo "<table border='2'>
-            <th> id</th>
-            <th>username</th>
-            <th>email</th>
-            <th>password</th>
-            <th>created_at</th>
-            <th>Action</th>";
-
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>
-            <td>".$row["id"]."</td>
-            <td>".$row["username"]."</td>
-            <td>".$row["email"]."</td>
-            <td>".$row["password"]."</td>
-            <td>".$row["created_at"]."</td>
-            <td>Edit</td>
-            </tr>";
-        }
-
-    echo "</table>";
+   
 
 
 
